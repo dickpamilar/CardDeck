6053406b2c2c59a257f06b559213a3dfda8f23c9
dickpamilar <dickpamilar@gmail.com>
2015-05-11 17:48:09 +1000

    Initial commit

---
+++ app/src/main/java/com/holidaydiaries/carddeck_memorytrainer/Deck.java
@@ -0,0 +1,16 @@
+package com.holidaydiaries.carddeck_memorytrainer;
+
+/**
+ * Created by dick on 11/05/2015.
+ */
+public class Deck {
+    private static final String[] packOfCards = {
+            "crd1.png" , "crd2.png", "crd3.png", "crd4.png",
+            "crd5.png" , "crd6.png", "crd7.png", "crd8.png",
+    };
+
+    public Deck (int packCount) {
+        int deckSize = packCount * packOfCards.length;
+
+    }
+}
